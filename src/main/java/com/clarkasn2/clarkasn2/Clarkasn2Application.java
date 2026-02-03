57d361d3be3c94346137201f72d2f24d57485fdc
ClarkNoveros-SFUCS <cdn5@sfu.ca>
2026-02-03 14:54:46 -0800

    first commit

---
+++ src/main/java/com/clarkasn2/clarkasn2/Clarkasn2Application.java
@@ -0,0 +1,13 @@
+package com.clarkasn2.clarkasn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clarkasn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clarkasn2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarkasn2/clarkasn2/Clarkasn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarkasn2.clarkasn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clarkasn2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
