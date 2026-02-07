e272d693883dab3b2b98d50b1c41051a04a7fff1
ClarkNoveros-SFUCS <cdn5@sfu.ca>
2026-02-06 22:50:20 -0800

    main: added new dependency

--- src/main/java/com/clarkasn2/clarkasn2/models/Users.java
+++ src/main/java/com/clarkasn2/clarkasn2/models/Users.java
@@ -1,10 +1,8 @@
 package com.clarkasn2.clarkasn2.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="usersTable")
@@ -12,4 +10,23 @@ public class Users {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int uid;
+
+    @NotBlank(message = "Fill up name")
+    private String name;
+
+    private Role rolyType;
+
+    private int clarity;
+
+    private int niceness;
+
+    private int knowledgeableScore;
+
+    private String comment;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+
+
 }
