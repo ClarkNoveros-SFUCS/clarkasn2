5f750f41c5f66d595af531bf723ebe89f092802c
ClarkNoveros-SFUCS <cdn5@sfu.ca>
2026-02-10 00:19:02 -0800

    main: defined controller methods

--- src/main/java/com/clarkasn2/clarkasn2/controllers/UsersController.java
+++ src/main/java/com/clarkasn2/clarkasn2/controllers/UsersController.java
@@ -9,9 +9,17 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.clarkasn2.clarkasn2.models.Users;
+
+
+
+
+
 
 //users: passing a collection (list)
 //user: passing one object
@@ -28,31 +36,103 @@ public class UsersController {
         return "users/index";
     }
 
-    //link to create 
-    @GetMapping("users/create")
-    public String createButton(Model model) {
-        model.addAttribute("user", new Users());
-        return "users/create";
-    }
+
+    //-->DETAILS PAGE<--
 
     //link to details
     @GetMapping("users/details/{uid}")
-    public String detailsUser(@PathVariable("uid") int uid, Model model){
+    public String detailsUser(@PathVariable int uid, Model model, RedirectAttributes redirectAttributes){
         Users user = usersList.stream().filter(u->u.getUid() == uid).findFirst().orElse(null);
 
-        if (user == null) 
-        return "redirect:/users/index";
+        if (user == null){
+            redirectAttributes.addFlashAttribute("error", "User not in database");
+            return "redirect:/users/index";
+        }
 
         model.addAttribute("user",user);
         return "users/details";
     }
+    
+    //delete
+    @GetMapping("users/delete/{uid}")
+    public String deleteUser(@PathVariable int uid, RedirectAttributes redirectAttributes) {
+        boolean removed = usersList.removeIf(u -> u.getUid() == uid);
 
-    //DETAILS PAGE
+        if(removed){
+            redirectAttributes.addFlashAttribute("success", "User has been deleted!");
+        }
+        else{
+            redirectAttributes.addFlashAttribute("error", "User not in database.");
+        }
 
-    //CREATE PAGE
+        return "redirect:/users/index";
+    }
 
+    
+    //-->CREATE PAGE<--
+    
+    //show create form, link to create page
+    @GetMapping("users/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("user", new Users());
 
-    //EDIT PAGE
+        return "users/create";
+    }
+    
+    //submit create form 
+    @PostMapping("users/create")
+    public String submitCreateForm(@ModelAttribute Users user, RedirectAttributes redirectAttributes){
+
+        user.setUid(usersList.size() + 1);
+        usersList.add(user);
+
+        redirectAttributes.addFlashAttribute("success", "User created!");
+
+        return "redirect:/users/details" + user.getUid();
+    }
+
+    
+    //-->EDIT PAGE<--
+    //show edit form, link to edit page
+    @GetMapping("users/edit/{uid}")
+    public String showEditForm(@PathVariable int uid, Model model, RedirectAttributes redirectAttributes) {
+        
+        Users user = usersList.stream().filter(u -> u.getUid() == uid).findFirst().orElse(null);
+        
+        if (user == null){
+
+            redirectAttributes.addFlashAttribute("error", "User not in database.");
+            return "redirect:/users/index";
+        }
+
+        model.addAttribute("user",user);
+        return "users/edit";
+    }
+
+    //submit edit form 
+    @PostMapping("users/edit/{uid}")
+    public String submitEditForm(@PathVariable int uid, @ModelAttribute Users updatedUser, RedirectAttributes redirectAttributes) {
+        
+        Users user = usersList.stream().filter(u -> u.getUid() == uid).findFirst().orElse(null);
+
+        if(user == null){
+            redirectAttributes.addFlashAttribute("error", "User not in database.");
+            return "redirect:/users/index";
+        }
+
+        //edit new data
+        user.setName(updatedUser.getName());
+        user.setEmail(updatedUser.getEmail());
+        user.setRoleType(updatedUser.getRoleType());
+        user.setClarity(updatedUser.getClarity());
+        user.setNiceness(updatedUser.getNiceness());
+        user.setKnowledgeableScore(updatedUser.getKnowledgeableScore());
+        user.setComment(updatedUser.getComment());
+
+        redirectAttributes.addFlashAttribute("success", "Updated successfully.");
+
+        return "redirect:/users/details" + uid;
+    }
 
 
 
