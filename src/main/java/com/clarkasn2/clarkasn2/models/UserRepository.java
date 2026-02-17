96c49aaf3afddb738442761013d73d28dfd46301
ClarkNoveros-SFUCS <cdn5@sfu.ca>
2026-02-17 14:22:15 -0800

    main: dependency edits

---
+++ src/main/java/com/clarkasn2/clarkasn2/models/UserRepository.java
@@ -0,0 +1,7 @@
+package com.clarkasn2.clarkasn2.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  UserRepository extends JpaRepository<Users,Integer> {
+    
+}
