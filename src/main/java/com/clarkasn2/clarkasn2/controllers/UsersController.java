ae6a4470b36284dd19d4b4c87157b46c7e75ef24
ClarkNoveros-SFUCS <cdn5@sfu.ca>
2026-02-17 16:05:35 -0800

    main: testing errors response

--- src/main/java/com/clarkasn2/clarkasn2/controllers/UsersController.java
+++ src/main/java/com/clarkasn2/clarkasn2/controllers/UsersController.java
@@ -3,9 +3,9 @@ package com.clarkasn2.clarkasn2.controllers;
 //Author: Clark Noveros
 //For: CMPT276 Asn2
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.clarkasn2.clarkasn2.models.Role;
+import com.clarkasn2.clarkasn2.models.UserRepository;
 import com.clarkasn2.clarkasn2.models.Users;
 
 import jakarta.validation.Valid;
@@ -31,26 +31,14 @@ import jakarta.validation.Valid;
 @Controller
 public class UsersController {
 
-    private final List<Users> usersList = new ArrayList<>();
-
-    public UsersController(){
-        Users u1 = new Users();
-        u1.setUid(1);
-        u1.setName("Bobby Chan");
-        u1.setEmail("bobbyc@sfu.ca");
-        u1.setRoleType(Role.PROF);
-        u1.setClarity(9);
-        u1.setNiceness(10);
-        u1.setKnowledgeableScore(9);
-        u1.setComment("Excellent CMPT 276 professor.");
-
-        usersList.add(u1);
-    }
+    @Autowired 
+    private UserRepository userRepo;
 
     //-->INDEX PAGE<--
     @GetMapping("/users/index")
     public String getAllUsers(Model model){
-        model.addAttribute("users",usersList);
+        List<Users> users = userRepo.findAll();
+        model.addAttribute("users",users);
         return "users/index";
     }
 
@@ -60,7 +48,7 @@ public class UsersController {
     //link to details
     @GetMapping("/users/details/{uid}")
     public String detailsUser(@PathVariable int uid, Model model, RedirectAttributes redirectAttributes){
-        Users user = usersList.stream().filter(u->u.getUid() == uid).findFirst().orElse(null);
+        Users user = userRepo.findById(uid).orElse(null);
 
         if (user == null){
             redirectAttributes.addFlashAttribute("error", "User not in database");
@@ -74,14 +62,13 @@ public class UsersController {
     //delete
     @PostMapping("/users/delete/{uid}")
     public String deleteUser(@PathVariable int uid, RedirectAttributes redirectAttributes) {
-        boolean removed = usersList.removeIf(u -> u.getUid() == uid);
+        if(!userRepo.existsById(uid)){
+            redirectAttributes.addFlashAttribute("error", "User not in database");
+            return "redirect:/users/index";
+        }
 
-        if(removed){
-            redirectAttributes.addFlashAttribute("success", "User has been deleted!");
-        }
-        else{
-            redirectAttributes.addFlashAttribute("error", "User not in database.");
-        }
+        userRepo.deleteById(uid);
+        redirectAttributes.addFlashAttribute("success", "User has been deleted");
 
         return "redirect:/users/index";
     }
@@ -99,14 +86,13 @@ public class UsersController {
     
     //submit create form 
     @PostMapping("/users/create")
-    public String submitCreateForm(@ModelAttribute Users user, RedirectAttributes redirectAttributes, BindingResult result, Model model){
+    public String submitCreateForm(@Valid @ModelAttribute("user") Users user, BindingResult result, RedirectAttributes redirectAttributes){
 
         if (result.hasErrors()){
             return "users/create";
         }
-        user.setUid(usersList.size() + 1);
-        usersList.add(user);
-
+       
+        userRepo.save(user);
         redirectAttributes.addFlashAttribute("success", "User created!");
         return "redirect:/users/details/" + user.getUid();
     }
@@ -117,7 +103,7 @@ public class UsersController {
     @GetMapping("/users/edit/{uid}")
     public String showEditForm(@PathVariable int uid, Model model, RedirectAttributes redirectAttributes) {
         
-        Users user = usersList.stream().filter(u -> u.getUid() == uid).findFirst().orElse(null);
+        Users user = userRepo.findById(uid).orElse(null);
         
         if (user == null){
 
@@ -133,25 +119,17 @@ public class UsersController {
     @PostMapping("/users/edit/{uid}")
     public String submitEditForm(@PathVariable int uid, @Valid @ModelAttribute Users updatedUser, BindingResult result, RedirectAttributes redirectAttributes) {
         
-        Users user = usersList.stream().filter(u -> u.getUid() == uid).findFirst().orElse(null);
-
-        if(user == null){
-            redirectAttributes.addFlashAttribute("error", "User not in database.");
-            return "redirect:/users/index";
-        }
-
         if(result.hasErrors()){
             return "users/edit";
         }
 
-        //edit new data
-        user.setName(updatedUser.getName());
-        user.setEmail(updatedUser.getEmail());
-        user.setRoleType(updatedUser.getRoleType());
-        user.setClarity(updatedUser.getClarity());
-        user.setNiceness(updatedUser.getNiceness());
-        user.setKnowledgeableScore(updatedUser.getKnowledgeableScore());
-        user.setComment(updatedUser.getComment());
+        if(!userRepo.existsById(uid)){
+            redirectAttributes.addFlashAttribute("error", "User not in database.");
+            return "redirect:/users/index";
+        }
+
+        updatedUser.setUid(uid);
+        userRepo.save(updatedUser);
 
         redirectAttributes.addFlashAttribute("success", "Updated successfully.");
 
